747b8d3c3d772d6b6330c225913786e86c8f630d
Revellin <99839050+Revellin@users.noreply.github.com>
2023-03-27 16:08:02 -0300

    Update index.php

--- Página Web/index.php
+++ Página Web/index.php
@@ -1,81 +1,83 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="pt-br">
 
 <head>
-  <link rel="stylesheet" href="style.css">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
-    crossorigin="anonymous"></script>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>SISTEMA WEB </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>php cadastro</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js "
+        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3 " crossorigin="anonymous ">
+    </script>
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar bg-primary" data-bs-theme="dark">
-   <div class="row">
-     <div class="col">
-      <div class="container-fluid">
-        <a class="navbar-brand nav-text" href="#">SISTEMA WEB</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Cadrastar</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled">Consultar</a>
-            </li>
-          </ul>
+
+    <div class="d-flex flex-column wrapper" >
+    <nav class="navbar navbar-expand-lg navbar bg-primary" data-bs-theme="dark">
+              <div class="container">
+                         <a class="navbar-brand text-white" href="/">SISTEMA WEB</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target=".navbar-collapse">
+                         <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse">
+                        <ul class="navbar-nav flex-grow-1">
+                            <li class="nav-item">
+                                 <a class="nav-link text-white active " href="/">Cadastrar</a>
+                            </li>
+                            <li class="nav-item">
+                                 <a class="nav-link text-white" href="/index.html">Consultar</a>
+                            </li>
+                        </ul>
+                     </div>
+                </div>
+        </nav>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+            </div>
         </div>
-      </div>
-     </div>
-   </div>
-  </nav>
-  <br>
-  <div class="row">
-    <div class="col">
-      <br>
-      <h4 class="h5-text"> Cadastrar - Agendamento de Potenciais Clientes</h4>
-      <h6 class="p-text">Sistema ultilizado para Agendamento de serviços.</h6>
-      <form class="form-text form-caixa" method="POST">
-        <br>
-        <h6>Nome:</h6>
-        <input class=" form-control  " type=" text " id=" txtNome "> </input>
-        <br>
-        <h6> Telefone: </h6>
-        <input class=" form-control " type=" text " id=" txtNome " placeholder="(xx)xxxxx-xxxx " autofocus></input>
-        </div>
-        <br>
-        <h6>Origem:</h6>
-        <select class="form-select" aria-label="Default select example">
-          <option selected>Celular</option>
-        </select>
-        <br>
-        <h6>Data de Contato:</h6>
-        <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-        </input>
-        <br>
-        <h6>Observação:</h6>
-        <textarea class="form-control" placeholder="" id="floatingTextareaDisabled"></textarea>
-        <label for="floatingTextarea2"></label>
-        <br>
-        <button type="button" class="btn btn-primary " data-bs-toggle="button" aria-pressed="true">Cadrastar</button>
-      
-      </form>
+        <main class="flex-fill">
+            <div class="container">
+                <div class="texto">
+                  <br>
+                    <h1>Cadastrar - Agendamento de Potenciais Clientes</h1>
+                  Sistema utilizado para agendamento de serviços
+                </div>
+                
+              <form class=" mt-3 " method="POST" action="teste.php">
+                          
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Nome</label>
+                                    <input type="text" class="form-control" id="exampleFormControlInput1" name="nome">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Telefone</label>
+                                    <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="(xx) xxxxxx-xxxx" name ="telefone">
+                                </div>
+
+                                <legenda>Origem</legenda>
+                                <select class="form-select" aria-label="Default select example" name="origem">
+                                    <option selected>Celular</option>
+                                    <opition value="1">Telefone Fixo</opition>
+                                </select>
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Data do contato</label>
+                                    <input type="date" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="observação">
+                                  </div>
+                                  <div class="mb-3">
+                                    <label for="exampleFormControlTextarea1" class="form-label">Observação</label>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                  </div>
+                         
+                                  <input type="submit" value="Cadastrar" class="btn btn-primary">
+                </form>
+            </div>
+        </main>
     </div>
-  </div>
-
-
-
-
-
+    </div>
 </body>
 
 </html>
